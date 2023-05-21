ed905c9b979b3b32d0f487c564d53415fc8883bb
ToWipf <TobF@web.de>
2023-05-21 17:47:48 +0200

    draw

--- src/main/java/org/wipf/puzzlewipf/logic/puzzle/PuzzleMain.java
+++ src/main/java/org/wipf/puzzlewipf/logic/puzzle/PuzzleMain.java
@@ -18,15 +18,49 @@ public class PuzzleMain {
 	public void doTest() throws IOException {
 		System.out.println("START");
 
-		SVGGraphics2D g2 = new SVGGraphics2D(500, 500);
+		Integer height_y = 4;
+		Integer weight_x = 3;
+		Integer scale = 10;
 
-		g2.drawRect(0, 0, 500, 500);
+		SVGGraphics2D g2 = new SVGGraphics2D(weight_x * scale, height_y * scale);
 
-		g2.drawLine(0, 0, 500, 500);
+		// Rahmen
+		// g2.drawRect(0, 0, height_y* scale, height_y * scale);
+
+//		for (int i = 0; i < height_y; i++) {
+//
+//			g2.drawLine(i * scale, 0, i * scale, weight_x * scale);
+//
+//		}
+		for (int x = 0; x < weight_x; x++) {
+			for (int y = 0; y < height_y; y++) {
+				System.out.println("Quadrat X: " + x + " Y: " + y);
+
+				g2 = doDrawVert(g2, x * scale, y * scale, scale);
+				g2 = doDrawHor(g2, x * scale, y * scale, scale);
+
+				g2 = doDrawVert(g2, (x * scale) + scale, (y * scale) + scale, -scale);
+				g2 = doDrawHor(g2, (x * scale) + scale, (y * scale) + scale, -scale);
+
+			}
+		}
 
 		File f = new File("files/test1.svg");
 
 		SVGUtils.writeToSVG(f, g2.getSVGElement());
+	}
+
+	private SVGGraphics2D doDrawVert(SVGGraphics2D g, int x, int y, int len) {
+		// 0 0 10 0
+		// 10 0 20 0
+		// 30 0 40 0
+		g.drawLine(x, y, len + x, y);
+		return g;
+	}
+
+	private SVGGraphics2D doDrawHor(SVGGraphics2D g, int x, int y, int len) {
+		g.drawLine(x, y, x, len + y);
+		return g;
 	}
 
 }
